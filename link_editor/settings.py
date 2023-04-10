13623035f2e15a23f558a230fe26ae47eedd051c
Ivan <ext-ivan.vasilevskiy@sbermarket.ru>
2023-04-10 10:18:29 +0300

    Add cors

--- link_editor/settings.py
+++ link_editor/settings.py
@@ -135,4 +135,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5137"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://127.0.0.1:5137", "http://158.160.29.67:5137", "http://158.160.29.67", "http://158.160.29.67:8000"]
